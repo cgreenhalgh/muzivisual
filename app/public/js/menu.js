a386f0c481b9dddca37b7c36751e5dd5c276c7ea
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2017-08-10 13:39:14 +0100

    update maps for london; add menu page alert about research

--- app/public/js/menu.js
+++ app/public/js/menu.js
@@ -24,7 +24,37 @@ menu.controller('menuCtrl', ['$scope', '$location', 'socket', '$window', '$ancho
     mpmLoguse.view('/', {});
     $scope.performing = false;
     $scope.archiveHighlight = false;
+    
+	// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+	function createCookie(name,value,days) {
+	    var expires = "";
+	    if (days) {
+	        var date = new Date();
+	        date.setTime(date.getTime() + (days*24*60*60*1000));
+	        expires = "; expires=" + date.toUTCString();
+	    }
+	    document.cookie = name + "=" + value + expires + "; path=/";
+	}
 
+	function readCookie(name) {
+	    var nameEQ = name + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0;i < ca.length;i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	    }
+	    return null;
+	}
+	var COOKIE_NAME = 'muzivisual-hidestudyalert';
+	var DEFAULT_DAYS = 365;
+
+    $scope.showStudyAlert = !readCookie(COOKIE_NAME);
+    $scope.hideStudyAlert = function() {
+    	$scope.showStudyAlert = false;
+    	createCookie(COOKIE_NAME,"1",DEFAULT_DAYS);
+    }
+    
     $scope.goToMenu = function () { $location.path('/'); };
 
     socket.on('vStart', function () {
